196fd703d827c733f63787c2f9ab628bd0361d45
karolpaul-design <pavelchycherin@gmail.com>
2022-04-13 01:39:44 +0300

    make it better

--- controllers/signIn.js
+++ controllers/signIn.js
@@ -20,7 +20,7 @@ const handleSignIn = async (req, res, prisma, bcrypt) => {
           email: email,
         },
       });
-      res.json(user);
+      return res.json(user);
     } else {
       res.status(400).json("wrong password");
     }
