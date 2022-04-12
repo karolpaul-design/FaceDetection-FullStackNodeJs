470c97b41009571661403f946fdcbe9cbd0347ad
karolpaul-design <pavelchycherin@gmail.com>
2022-04-13 01:30:50 +0300

    make it better

--- controllers/signIn.js
+++ controllers/signIn.js
@@ -5,27 +5,22 @@ const handleSignIn = async (req, res, prisma, bcrypt) => {
   if (!email || !password) {
     return res.status(400).json("incorrect form submission");
   }
-  const emailValidation = await prisma.users.findUnique({
+  const emailValidation = await prisma.login.findUnique({
     where: {
       email: email,
     },
   });
-  res.json(emailValidation);
+
   if (emailValidation) {
     const hash = emailValidation.hash;
     const passwordValidation = bcrypt.compareSync(password, hash);
     if (passwordValidation) {
-      try {
-        const user = await prisma.users.findUnique({
-          where: {
-            id: 2,
-          },
-        });
-        res.json(user);
-      } catch (e) {
-        response.status(400).json(e);
-      }
-      res.json("user2");
+      const user = await prisma.users.findUnique({
+        where: {
+          email: email,
+        },
+      });
+      res.json(user);
     } else {
       res.status(400).json("wrong password");
     }
